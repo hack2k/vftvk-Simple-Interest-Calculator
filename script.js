89ead0b419de5542e70484327708a54c723cd858
hack2k <43598610+hack2k@users.noreply.github.com>
2021-04-17 10:50:35 +0530

    Add files via upload

--- script.js
+++ script.js
@@ -1,15 +1,23 @@
 function compute()
 {
     var principal = document.getElementById("principal").value;
+    if (principal<=0)
+    {
+        document.getElementById("principal").focus();
+        alert("enter a positive number");
+    }
+    else
+    {
     var rate = document.getElementById("rate").value;
     var years = document.getElementById("years").value;
     var interest = principal * years * rate /100;
     var year = new Date().getFullYear()+parseInt(years);
     var ans=document.getElementById("result");
-    ans.innerHTML="if you deposit "+ principal  +"<br>"+
-                " at an intrest rate of " + rate +"<br>"+
-                "you will receive an amount of "+ interest +"<br>"+
-                "in the year "+year;
+    ans.innerHTML="if you deposit <mark>"+ principal  +"</mark><br>"+
+                " at an intrest rate of <mark>" + rate +"</mark><br>"+
+                "you will receive an amount of <mark>"+ interest +"</mark><br>"+
+                "in the year <mark>"+year +"</mark>";
+    }
 
 
     
